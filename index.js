173f13e6b99cbc9f202ad7e750ccb4d130cc1ef4
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-15 16:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let divide = (parameter1,parameter2) => parameter1 / parameter2
 divide(2000,100); //=> 5
 
-(number1, number2) => number1 * number2;
+let square = (param1) => param1 ** 2
+square(5);
+
+let add = (param1,param2) => param1 + param2
+console.log(add(5,12)) 
