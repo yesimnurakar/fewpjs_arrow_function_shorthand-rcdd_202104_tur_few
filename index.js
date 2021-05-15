d7fab775ab658d913ad4c431308dd1880592574c
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-15 16:21:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let divide = (parameter1, parameter2) => parameter1 / parameter2;
+let divide = (parameter1,parameter2) => parameter1 / parameter2
 divide(2000,100); //=> 5
 
 (number1, number2) => number1 * number2;
